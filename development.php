cd0c1492dd9c0857d77c725ad386700295c6bd54
Meghan P <mpatric3@u.rochester.edu>
2018-04-22 20:10:38 -0400

    AI Initial Set Up
    Added file structure and tags

---
+++ development.php
@@ -0,0 +1,53 @@
+<?php include "inc/html-top.inc";?>
+    
+    <body>
+        <section class = "banner">
+            <?php include "inc/header.inc"; ?>
+            <?php include "inc/nav.inc"; ?>
+        </section> <!--.banner-->
+
+        <div class = "container">
+        
+        <article>
+            
+             <h2>The Development of Fruit</h2>
+
+                    <figure> 
+                        <img class="scale" src="images/devoffruit.jpg" alt="Development of Fruit">
+                     <figcaption> The development sequence of a typical drupe, the nectarine (Prunus persica) over a 7.5 month period, from bud formation in early winter to fruit ripening in midsummer (see image page for further information)
+                    </figcaption>
+                    </figure>
+            
+            <div class="fruit_info">
+
+                <h3>Where Does Fruit Come From?</h3>
+                <p>
+                A fruit results from maturation of one or more flowers, and the gynoecium of the flower(s) forms all or part of the fruit.[10]
+                </p>
+                <p>
+                Inside the ovary/ovaries are one or more ovules where the megagametophyte contains the egg cell.[11] After double fertilization, these ovules will become seeds. The ovules are fertilized in a process that starts with pollination, which involves the movement of pollen from the stamens to the stigma of flowers. After pollination, a tube grows from the pollen through the stigma into the ovary to the ovule and two sperm are transferred from the pollen to the megagametophyte. Within the megagametophyte one of the two sperm unites with the egg, forming a zygote, and the second sperm enters the central cell forming the endosperm mother cell, which completes the double fertilization process.[12][13] Later the zygote will give rise to the embryo of the seed, and the endosperm mother cell will give rise to endosperm, a nutritive tissue used by the embryo.
+                </p>
+                <p>
+                As the ovules develop into seeds, the ovary begins to ripen and the ovary wall, the pericarp, may become fleshy (as in berries or drupes), or form a hard outer covering (as in nuts). In some multiseeded fruits, the extent to which the flesh develops is proportional to the number of fertilized ovules.[14] The pericarp is often differentiated into two or three distinct layers called the exocarp (outer layer, also called epicarp), mesocarp (middle layer), and endocarp (inner layer). In some fruits, especially simple fruits derived from an inferior ovary, other parts of the flower (such as the floral tube, including the petals, sepals, and stamens), fuse with the ovary and ripen with it. In other cases, the sepals, petals and/or stamens and style of the flower fall off. When such other floral parts are a significant part of the fruit, it is called an accessory fruit. Since other parts of the flower may contribute to the structure of the fruit, it is important to study flower structure to understand how a particular fruit forms.[3]
+                </p>
+
+                <h3>Modes of Fruit Development</h3>
+                <p>
+                Apocarpous fruits develop from a single flower having one or more separate carpels, and they are the simplest fruits. Syncarpous fruits develop from a single gynoecium having two or more carpels fused together.
+                Multiple fruits form from many different flowers. Plant scientists have grouped fruits into three main groups, simple fruits, aggregate fruits, and composite or multiple fruits.[15] The groupings are not evolutionarily relevant, since many diverse plant taxa may be in the same group, but reflect how the flower organs are arranged and how the fruits develop.
+                </p>
+                <p>
+                Simple fruits can be either dry or fleshy, and result from the ripening of a simple or compound ovary in a flower with only one pistil. Dry fruits may be either dehiscent (they open to discharge seeds), or indehiscent (they do not open to discharge seeds).[16] Types of dry, simple fruits, and examples of each, include:
+                </p>
+
+        </div>       
+        </article>
+
+
+        <?php include "inc/footer.inc";?>
+
+        </div><!--.container -->
+
+        <?php include "inc/scripts.inc";?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include "inc/html-top.inc";?>
+	<body>
+		<section class = "banner">
+            <?php include "inc/header.inc"; ?>
+            <?php include "inc/nav.inc"; ?>
+        </section> <!--.banner-->
+
+		<div class= "container" style="z-index: 1;">
+
+			<div class = "slider" style="z-index: -1;"> <!--slider of images of place where each person is from-->
+	        	<img src="images/fruitpic.jpg" alt="A Nice Fruit Arrangement.">
+				<img src="images/morefruit.jpg" alt="Even more fruit.">
+				<img src="images/seedlessfruit.jpg" alt="Seedless Fruit">
+			</div>
+
+			<div>
+				<h2>Fruits and Berries</h2>
+				<p>In botany, a fruit is the seed-bearing structure in flowering plants (also known as angiosperms) formed from the ovary after flowering.
+				</p>
+
+				<p>Fruits are the means by which angiosperms disseminate seeds. Edible fruits, in particular, have propagated with the movements of humans and animals in a symbiotic relationship as a means for seed dispersal and nutrition; in fact, humans and many animals have become dependent on fruits as a source of food.[1] Accordingly, fruits account for a substantial fraction of the world's agricultural output, and some (such as the apple and the pomegranate) have acquired extensive cultural and symbolic meanings.
+				</p>
+
+				<p>In common language usage, "fruit" normally means the fleshy seed-associated structures of a plant that are sweet or sour, and edible in the raw state, such as apples, bananas, grapes, lemons, oranges, and strawberries. On the other hand, in botanical usage, "fruit" includes many structures that are not commonly called "fruits", such as bean pods, corn kernels, tomatoes, and wheat grains.[2][3] The section of a fungus that produces spores is also called a fruiting body.[4]
+				</p>
+
+				<h2>Culinary Fruit and Botanical Fruit</h2>
+				<p>
+				Many common terms for seeds and fruit do not correspond to the botanical classifications. In culinary terminology, a fruit is usually any sweet-tasting plant part, especially a botanical fruit; a nut is any hard, oily, and shelled plant product; and a vegetable is any savory or less sweet plant product.[5] However, in botany, a fruit is the ripened ovary or carpel that contains seeds, a nut is a type of fruit and not a seed, and a seed is a ripened ovule.[6]
+				</p>
+				<p>
+				Examples of culinary "vegetables" and nuts that are botanically fruit include corn, cucurbits (e.g., cucumber, pumpkin, and squash), eggplant, legumes (beans, peanuts, and peas), sweet pepper, and tomato. In addition, some spices, such as allspice and chili pepper, are fruits, botanically speaking.[6] In contrast, rhubarb is often referred to as a fruit, because it is used to make sweet desserts such as pies, though only the petiole (leaf stalk) of the rhubarb plant is edible,[7] and edible gymnosperm seeds are often given fruit names, e.g., ginkgo nuts and pine nuts.
+				</p>
+				<p>
+				Botanically, a cereal grain, such as corn, rice, or wheat, is also a kind of fruit, termed a caryopsis. However, the fruit wall is very thin and is fused to the seed coat, so almost all of the edible grain is actually a seed.[8]
+				</p>
+			</div>
+
+			<?php include "inc/footer.inc"; ?>
+
+		</div> <!--.container -->
+		<?php include "inc/scripts.inc"; ?>
+	</body>
+</html>
