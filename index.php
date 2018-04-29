f7ec398f2298f05f41ef1418fb423a44299b19e0
Melanie DeJong <melaniedejong42@gmail.com>
2018-04-28 20:03:32 -0400

    Moved survey back

--- index.php
+++ index.php
@@ -1,44 +1,47 @@
 <?php include "inc/html-top.inc";?>
-	<body>
-            <?php include "inc/header.inc"; ?>
-            <?php include "inc/nav.inc"; ?>
-
-		<div class= "container" style="z-index: 1;">
-
-			<img src="images/fruitpic.jpg" alt="A Nice Fruit Arrangement.">
-
-			<div>
-				<h2>Fruit: An Overview</h2>
-
-				<h3>Fruits and Berries</h3>
-				<p>In botany, a fruit is the seed-bearing structure in flowering plants (also known as angiosperms) formed from the ovary after flowering.
-				</p>
-
-				<h3>Culinary Fruit and Botanical Fruit</h3>
-				<p>
-				Many common terms for seeds and fruit do not correspond to the botanical classifications. In culinary terminology, a fruit is usually any sweet-tasting plant part, especially a botanical fruit; a nut is any hard, oily, and shelled plant product; and a vegetable is any savory or less sweet plant product.[5] However, in botany, a fruit is the ripened ovary or carpel that contains seeds, a nut is a type of fruit and not a seed, and a seed is a ripened ovule.[6]
-				</p>
-
-			</div>
-
-
-			<div class = "survey">
-                <p><a href="survey.php">Take our survey!</a></p>
-
+<body>
+   <?php include "inc/header.inc"; ?>
+   <?php include "inc/nav.inc"; ?>
+   <div class= "container" style="z-index: 1;">
+      <img src="images/fruitpic.jpg" alt="A Nice Fruit Arrangement.">
+      <div>
+         <h2>Fruit: An Overview</h2>
+         <h3>Fruits and Berries</h3>
+         <p>In botany, a fruit is the seed-bearing structure in flowering plants (also known as angiosperms) formed from the ovary after flowering.
+         </p>
+         <h3>Culinary Fruit and Botanical Fruit</h3>
+         <p>
+            Many common terms for seeds and fruit do not correspond to the botanical classifications. In culinary terminology, a fruit is usually any sweet-tasting plant part, especially a botanical fruit; a nut is any hard, oily, and shelled plant product; and a vegetable is any savory or less sweet plant product.[5] However, in botany, a fruit is the ripened ovary or carpel that contains seeds, a nut is a type of fruit and not a seed, and a seed is a ripened ovule.[6]
+         </p>
+      </div>
+      <div class = "survey">
+         <form method="POST" action="thanks.php">
+            <div>
+               <h2>First Name</h2>
             </div>
-
-
-		    <div>
-		        <h2>Login</h2>
-		        <p>If you are an admin, please fill in your credentials to <a href="login.php">login.</a></p>
-		        <form>
-		            <p> Don't have an account? <a href="register.php">Sign up now</a>.</p>
-		        </form>
-		    </div> 
-
-			<?php include "inc/footer.inc"; ?>
-
-		</div> <!--.container -->
-		<?php include "inc/scripts.inc"; ?>
-	</body>
+            <input type="text" name="first_name" required>
+            <div>
+               <h2>Last Name</h2>
+            </div>
+            <input type="text" name="last_name" required>
+            <div>
+               <h2>What fruit would you like to learn more about?</h2>
+            </div>
+            <input type="text" name="new_fruit" required> </br></br>
+            <input type="submit" id="submitButton"
+               name="submitClick" value="Submit">
+         </form>
+      </div>
+      <div>
+         <h2>Login</h2>
+         <p>If you are an admin, please fill in your credentials to <a href="login.php">login.</a></p>
+         <form>
+            <p> Don't have an account? <a href="register.php">Sign up now</a>.</p>
+         </form>
+      </div>
+      <?php include "inc/footer.inc"; ?>
+   </div>
+   <!--.container -->
+   <?php include "inc/scripts.inc"; ?>
+</body>
 </html>
