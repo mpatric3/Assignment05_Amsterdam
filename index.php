9a9a011e955c039973a7d07dd4d8f76f78fc236e
Melanie DeJong <melaniedejong42@gmail.com>
2018-04-30 01:36:41 -0400

    Fixed some errors, resolved branch conflicts

--- index.php
+++ index.php
@@ -1,99 +1,53 @@
 <?php include "inc/html-top.inc";?>
-<<<<<<< HEAD
-	<article>
-	<body>
-            <?php include "inc/header.inc"; ?>
-            <?php include "inc/nav.inc"; ?>
-
-		<div class= "container" style="z-index: 1;">
-
-		<div>
-			<img class="scale" src="images/fruitpic.jpg" alt="A Nice Fruit Arrangement.">
-		</div>
-
-			<div>
-				<h2>Fruit: An Overview</h2>
-
-				<h3>Fruits and Berries</h3>
-				<p>In botany, a fruit is the seed-bearing structure in flowering plants (also known as angiosperms) formed from the ovary after flowering.
-				</p>
-
-				<h3>Culinary Fruit and Botanical Fruit</h3>
-				<p>
-				Many common terms for seeds and fruit do not correspond to the botanical classifications. In culinary terminology, a fruit is usually any sweet-tasting plant part, especially a botanical fruit; a nut is any hard, oily, and shelled plant product; and a vegetable is any savory or less sweet plant product.[5] However, in botany, a fruit is the ripened ovary or carpel that contains seeds, a nut is a type of fruit and not a seed, and a seed is a ripened ovule.[6]
-				</p>
-
-			</div>
-			</article>
-
-
-			<aside>
-			<div class = "survey">
-                <h1>What fruit would you like to learn more about?</h1>
-                    <form method = "post" action = "thanks.php">
-                    </form>
-            </div>
-
-
-		    <div class = "login">
-		        <h2>Login</h2>
-		        <p>If you are an admin, please fill in your credentials to login.</p>
-		        <form>
-		            <p> Don't have an account? <a href="register.php">Sign up now</a>.</p>
-		        </form>
-		    </div>
-		</aside>
-
-			<?php include "inc/footer.inc"; ?>
-
-		</div> <!--.container -->
-		<?php include "inc/scripts.inc"; ?>
-	</body>
-=======
 <body>
-   <?php include "inc/header.inc"; ?>
+<?php include "inc/header.inc"; ?>
    <?php include "inc/nav.inc"; ?>
-   <div class= "container" style="z-index: 1;">
-      <img src="images/fruitpic.jpg" alt="A Nice Fruit Arrangement.">
+   <div class= "container">
       <div>
-         <h2>Fruit: An Overview</h2>
-         <h3>Fruits and Berries</h3>
-         <p>In botany, a fruit is the seed-bearing structure in flowering plants (also known as angiosperms) formed from the ovary after flowering.
-         </p>
-         <h3>Culinary Fruit and Botanical Fruit</h3>
-         <p>
-            Many common terms for seeds and fruit do not correspond to the botanical classifications. In culinary terminology, a fruit is usually any sweet-tasting plant part, especially a botanical fruit; a nut is any hard, oily, and shelled plant product; and a vegetable is any savory or less sweet plant product.[5] However, in botany, a fruit is the ripened ovary or carpel that contains seeds, a nut is a type of fruit and not a seed, and a seed is a ripened ovule.[6]
-         </p>
+         <img class="scale" src="images/fruitpic.jpg" alt="A Nice Fruit Arrangement.">
       </div>
-      <div class = "survey">
-         <form method="POST" action="thanks.php">
-            <div>
-               <h2>First Name</h2>
-            </div>
-            <input type="text" name="first_name" required>
-            <div>
-               <h2>Last Name</h2>
-            </div>
-            <input type="text" name="last_name" required>
-            <div>
-               <h2>What fruit would you like to learn more about?</h2>
-            </div>
-            <input type="text" name="new_fruit" required> </br></br>
-            <input type="submit" id="submitButton"
-               name="submitClick" value="Submit">
-         </form>
-      </div>
-      <div class="login">
-         <h2>Login</h2>
-         <p>If you are an admin, please fill in your credentials to <a href="login.php">login.</a></p>
-         <form>
-            <p> Don't have an account? <a href="register.php">Sign up now</a>.</p>
-         </form>
-      </div>
+      <article>
+         <div>
+            <h2>Fruit: An Overview</h2>
+            <h3>Fruits and Berries</h3>
+            <p>In botany, a fruit is the seed-bearing structure in flowering plants (also known as angiosperms) formed from the ovary after flowering.
+            </p>
+            <h3>Culinary Fruit and Botanical Fruit</h3>
+            <p>
+               Many common terms for seeds and fruit do not correspond to the botanical classifications. In culinary terminology, a fruit is usually any sweet-tasting plant part, especially a botanical fruit; a nut is any hard, oily, and shelled plant product; and a vegetable is any savory or less sweet plant product.[5] However, in botany, a fruit is the ripened ovary or carpel that contains seeds, a nut is a type of fruit and not a seed, and a seed is a ripened ovule.[6]
+            </p>
+         </div>
+      </article>
+      <aside>
+         <div class = "survey">
+            <form method="POST" action="thanks.php">
+               <div>
+                  <h2>First Name</h2>
+               </div>
+               <input type="text" name="first_name" required>
+               <div>
+                  <h2>Last Name</h2>
+               </div>
+               <input type="text" name="last_name" required>
+               <div>
+                  <h2>What fruit would you like to learn more about?</h2>
+               </div>
+               <input type="text" name="new_fruit" required> <br><br>
+               <input type="submit" id="submitButton"
+                  name="submitClick" value="Submit">
+            </form>
+         </div>
+         <div class = "login">
+            <h2>Login</h2>
+            <p>If you are an admin, please fill in your credentials to login.</p>
+            <form>
+               <p> Don't have an account? <a href="register.php">Sign up now</a>.</p>
+            </form>
+         </div>
+      </aside>
       <?php include "inc/footer.inc"; ?>
    </div>
    <!--.container -->
    <?php include "inc/scripts.inc"; ?>
 </body>
->>>>>>> af45e4dd63ea7cab0c54357aa92c99c7fe8f9516
 </html>
--- uses.php
+++ uses.php
@@ -1,47 +1,34 @@
 <?php include "inc/html-top.inc";?>
-    
-    <body>
-        <section class = "banner">
-            <?php include "inc/header.inc"; ?>
-            <?php include "inc/nav.inc"; ?>
-        </section> <!--.banner-->
-
-        <div class = "container">
-        
-        <article>
-
-            <h2>Uses for Fruit</h2>
-
-                    <figure class="place"> 
-                        <img class="scale" src="images/squash.jpg" alt="Fruit Arrangement">
-                     <figcaption> An arrangement of fruits commonly thought of as vegetables, including tomatoes and various squash</figcaption>
-                    </figure>
-
-            <div class="fruit_info">
-                <h3>Nutritional Value</h3>
-                    <p>Fresh fruits are generally high in fiber, vitamin C, and water.[33] Regular consumption of fruit is generally associated with reduced risks of several diseases and functional declines associated with aging.[34][35]
-                    </p>
-                    <p>Many hundreds of fruits, including fleshy fruits (like apple, kiwifruit, mango, peach, pear, and watermelon) are commercially valuable as human food, eaten both fresh and as jams, marmalade and other preserves. Fruits are also used in manufactured foods (e.g., cakes, cookies, ice cream, muffins, or yogurt) or beverages, such as fruit juices (e.g., apple juice, grape juice, or orange juice) or alcoholic beverages (e.g., brandy, fruit beer, or wine).[30] Fruits are also used for gift giving, e.g., in the form of Fruit Baskets and Fruit Bouquets.
-                    </p>
-                    <p>Many "vegetables" in culinary parlance are botanical fruits, including bell pepper, cucumber, eggplant, green bean, okra, pumpkin, squash, tomato, and zucchini.[31] Olive fruit is pressed for olive oil. Spices like allspice, black pepper, paprika, and vanilla are derived from berries.[32]
-                    </p>
-
-                 <h3>Nonfood Uses</h3>
-                    <p>Because fruits have been such a major part of the human diet, various cultures have developed many different uses for fruits they do not depend on for food.</p>
-                    </p>
-                    <p>Bayberry fruits provide a wax often used to make candles;[36] Many dry fruits are used as decorations or in dried flower arrangements (e.g., annual honesty, cotoneaster, lotus, milkweed, unicorn plant, and wheat). Ornamental trees and shrubs are often cultivated for their colorful fruits, including beautyberry, cotoneaster, holly, pyracantha, skimmia, and viburnum.[37] Fruits of opium poppy are the source of opium, which contains the drugs codeine and morphine, as well as the biologically inactive chemical theabaine from which the drug oxycodone is synthesized.[38] Osage orange fruits are used to repel cockroaches.[39]
-                    </p>
-                    <p>Many fruits provide natural dyes (e.g., cherry, mulberry, sumac, and walnut).[40] Dried gourds are used as bird houses, cups, decorations, dishes, musical instruments, and water jugs. Pumpkins are carved into Jack-o'-lanterns for Halloween. The spiny fruit of burdock or cocklebur inspired the invention of Velcro.[41] Coir fiber from coconut shells is used for brushes, doormats, floor tiles, insulation, mattresses, sacking, and as a growing medium for container plants. The shell of the coconut fruit is used to make bird houses, bowls, cups, musical instruments, and souvenir heads.[42]
-                    </p>
-
-
-        </div>
-        </article>
-
-        <?php include "inc/footer.inc";?>
-
-        </div><!--.container -->
-        
-        <?php include "inc/scripts.inc";?>
-    </body>
+<body>
+   <?php include "inc/header.inc"; ?>
+   <?php include "inc/nav.inc"; ?>
+   <div class = "container">
+      <article>
+         <h2>Uses for Fruit</h2>
+         <figure class="place">
+            <img class="scale" src="images/squash.jpg" alt="Fruit Arrangement">
+            <figcaption> An arrangement of fruits commonly thought of as vegetables, including tomatoes and various squash</figcaption>
+         </figure>
+         <div class="fruit_info">
+            <h3>Nutritional Value</h3>
+            <p>Fresh fruits are generally high in fiber, vitamin C, and water.[33] Regular consumption of fruit is generally associated with reduced risks of several diseases and functional declines associated with aging.[34][35]
+            </p>
+            <p>Many hundreds of fruits, including fleshy fruits (like apple, kiwifruit, mango, peach, pear, and watermelon) are commercially valuable as human food, eaten both fresh and as jams, marmalade and other preserves. Fruits are also used in manufactured foods (e.g., cakes, cookies, ice cream, muffins, or yogurt) or beverages, such as fruit juices (e.g., apple juice, grape juice, or orange juice) or alcoholic beverages (e.g., brandy, fruit beer, or wine).[30] Fruits are also used for gift giving, e.g., in the form of Fruit Baskets and Fruit Bouquets.
+            </p>
+            <p>Many "vegetables" in culinary parlance are botanical fruits, including bell pepper, cucumber, eggplant, green bean, okra, pumpkin, squash, tomato, and zucchini.[31] Olive fruit is pressed for olive oil. Spices like allspice, black pepper, paprika, and vanilla are derived from berries.[32]
+            </p>
+            <h3>Nonfood Uses</h3>
+            <p>Because fruits have been such a major part of the human diet, various cultures have developed many different uses for fruits they do not depend on for food.
+            </p>
+            <p>Bayberry fruits provide a wax often used to make candles;[36] Many dry fruits are used as decorations or in dried flower arrangements (e.g., annual honesty, cotoneaster, lotus, milkweed, unicorn plant, and wheat). Ornamental trees and shrubs are often cultivated for their colorful fruits, including beautyberry, cotoneaster, holly, pyracantha, skimmia, and viburnum.[37] Fruits of opium poppy are the source of opium, which contains the drugs codeine and morphine, as well as the biologically inactive chemical theabaine from which the drug oxycodone is synthesized.[38] Osage orange fruits are used to repel cockroaches.[39]
+            </p>
+            <p>Many fruits provide natural dyes (e.g., cherry, mulberry, sumac, and walnut).[40] Dried gourds are used as bird houses, cups, decorations, dishes, musical instruments, and water jugs. Pumpkins are carved into Jack-o'-lanterns for Halloween. The spiny fruit of burdock or cocklebur inspired the invention of Velcro.[41] Coir fiber from coconut shells is used for brushes, doormats, floor tiles, insulation, mattresses, sacking, and as a growing medium for container plants. The shell of the coconut fruit is used to make bird houses, bowls, cups, musical instruments, and souvenir heads.[42]
+            </p>
+         </div>
+      </article>
+      <?php include "inc/footer.inc";?>
+   </div>
+   <!--.container -->
+   <?php include "inc/scripts.inc";?>
+</body>
 </html>
